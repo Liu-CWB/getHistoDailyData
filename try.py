5a83d0b4e563d3defbb2dbc476a98bb9f211ddfb
Liu-CWB <34766410+Liu-CWB@users.noreply.github.com>
2018-09-18 13:09:15 +0800

    Create try.py

---
+++ try.py
@@ -0,0 +1,6 @@
+#This script is written for the person who want to get CWB historical daily data. 
+#However, this script is not for web crawler so that you should have the data before you excute this script.
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
